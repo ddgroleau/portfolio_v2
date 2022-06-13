b112cb67518c78b91bedca8c619114bbd537dd35
Dan Groleau <ddgroleau@gmail.com>
2022-06-12 20:32:56 -0600

    Added title/meta to page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import projects from '../projects';
 import ProjectCard from '../components/project-card/ProjectCard';
 import About from '../components/about/About';
 import Blog from '../components/blog/Blog';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
     const [isMobileToggled, setIsMobileToggled] = useState<boolean>(false);
@@ -41,60 +42,68 @@ const Home: NextPage = () => {
     };
 
     return (
-        <main>
-            <section className={styles.container}>
-                <header className={styles.header}>
-                    <div className={styles.logo}>
-                        <Logo height={"100%"} width={"100%"}/>
-                    </div>
-                    <div className={styles.headerContent}>
-                        <div className={styles.socialIcons}>
-                            <strong>
-                                <Link href="/Dan_Groleau_Resume.pdf" passHref>
-                                    <a target="_blank">RESUME</a>
-                                </Link>
-                            </strong>
-                            <GitHubIcon/>
-                            <EmailIcon/>
-                            <LinkedInIIcon/>
+        <>      
+            <Head>
+                <title>Dan Groleau | Portfolio</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="description" content="The professional software engineering portfolio of Dan Groleau." />
+            </Head>
+            <main>
+                <section className={styles.container}>
+                    <header className={styles.header}>
+                        <div className={styles.logo}>
+                            <Logo height={"100%"} width={"100%"}/>
                         </div>
-                        <div>
-                            <h1>Dan Groleau</h1>
-                            <p>Software Engineer</p>
+                        <div className={styles.headerContent}>
+                            <div className={styles.socialIcons}>
+                                <strong>
+                                    <Link href="/Dan_Groleau_Resume.pdf" passHref>
+                                        <a target="_blank">RESUME</a>
+                                    </Link>
+                                </strong>
+                                <GitHubIcon/>
+                                <EmailIcon/>
+                                <LinkedInIIcon/>
+                            </div>
+                            <div>
+                                <h1>Dan Groleau</h1>
+                                <p>Software Engineer</p>
+                            </div>
                         </div>
-                    </div>
-                </header>
-                <section className={styles.body}>
-                    <Animation/>
-                    <div className={styles.carousel}>
-                        <p className={styles.carouselTitle}><strong>{menuItems[activeIndex].text}</strong></p>
-                        {menuItems[activeIndex].component}
-                    </div>
-                </section>
-                <section className={styles.menu}>
-                    <div className={styles.mobileMenuToggle}>
-                        <DoubleArrowButton 
-                            onClick={handleOpenMobileFilters} 
-                            rotateDegrees={isMobileToggled ? 90 : -90}/>
-                    </div>
-                    <div 
-                        className={`${isMobileToggled ? styles.menuButtonsOpen : styles.menuButtonsClosed} 
+                    </header>
+                    <section className={styles.body}>
+                        <Animation/>
+                        <div className={styles.carousel}>
+                            <p className={styles.carouselTitle}><strong>{menuItems[activeIndex].text}</strong></p>
+                            {menuItems[activeIndex].component}
+                        </div>
+                    </section>
+                    <section className={styles.menu}>
+                        <div className={styles.mobileMenuToggle}>
+                            <DoubleArrowButton 
+                                onClick={handleOpenMobileFilters} 
+                                rotateDegrees={isMobileToggled ? 90 : -90}/>
+                        </div>
+                        <div 
+                            className={`${isMobileToggled ? styles.menuButtonsOpen : styles.menuButtonsClosed} 
                             ${isAnimated ? styles.slideUp : ''}`}
-                        onAnimationEnd={()=>setIsAnimated(false)}
-                    >
-                        {menuItems.map(item => {
-                            return <DimensionalButton 
-                                key={menuItems.indexOf(item)} 
-                                isActive={menuItems.indexOf(item) === activeIndex}
-                                onClick={()=>handleClick(item)}
-                            >
-                                {item.text}
-                            </DimensionalButton>;
-                        })}
-                    </div>
+                            onAnimationEnd={()=>setIsAnimated(false)}
+                        >
+                            {menuItems.map(item => {
+                                return <DimensionalButton 
+                                    key={menuItems.indexOf(item)} 
+                                    isActive={menuItems.indexOf(item) === activeIndex}
+                                    onClick={()=>handleClick(item)}
+                                >
+                                    {item.text}
+                                </DimensionalButton>;
+                            })}
+                        </div>
+                    </section>
                 </section>
-            </section>
-        </main>
+            </main>
+        </>
+
     );
 };
 
